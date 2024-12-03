c32fe6e97bce6a57076b3bc05e041dec3891de9f
rizky-verandi <classieroof@gmail.com>
2024-12-03 08:05:34 +0700

    fix: button loadmore to show more items

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, Suspense } from "react";
+import { useEffect } from "react";
 import { fetchCats } from "./features/cats/catSlice";
 import { useAppSelector } from "./store/hooks";
 import { useDispatch } from "react-redux";
@@ -6,14 +6,20 @@ import Card from "./components/Card";
 import Container from "./components/Container";
 import { CatState } from "./features/cats/catSlice";
 import Loading from "./components/Loading";
+import { useState } from "react";
 function App() {
   const cats = useAppSelector((state) => state.cats);
   const dispatch = useDispatch();
+  const [page, setPage] = useState(8);
 
   useEffect(() => {
     dispatch(fetchCats());
   }, [dispatch]);
   console.log(cats);
+
+  const showMoreCats = () => {
+    setPage((prev) => prev + 8);
+  };
 
   if (cats.isLoading) {
     return <Loading />;
@@ -23,11 +29,11 @@ function App() {
     <section className="m-auto w-full mt-7">
       <Container>
         <div className="py-8">
-          <h1 className="text-5xl text-center py-8 font-medium">
+          <h1 className="text-5xl text-center py-8 font-medium  ">
             Cats Library
           </h1>
           <div className="grid grid-cols-4 gap-4 pb-8">
-            {cats.cats.slice(0, 8).map((cat: CatState, index: number) => (
+            {cats.cats.slice(0, page).map((cat: CatState, index: number) => (
               <Card
                 key={index}
                 imgUrl={`https://cdn2.thecatapi.com/images/${cat.reference_image_id}.jpg`}
@@ -36,9 +42,14 @@ function App() {
             ))}
           </div>
           <div className="text-center">
-            <button className="py-2 px-4 text-center bg-[#3b3b3b] text-white">
-              Load more
-            </button>
+            {cats.cats.length > page && (
+              <button
+                className="py-2 px-4 text-center bg-[#3b3b3b] text-white"
+                onClick={showMoreCats}
+              >
+                Load more
+              </button>
+            )}
           </div>
         </div>
       </Container>
--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -2,6 +2,5 @@ import { all } from "redux-saga/effects";
 import catSaga from "../features/cats/catSaga";
 
 export default function* rootSaga() {
-  console.log("rootSaga")
   yield all([catSaga()]);
 }
